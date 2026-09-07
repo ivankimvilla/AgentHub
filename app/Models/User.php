b69a250cedc0215baf7b3c874c28037c035e274a
ivankimvilla <ivanalmadin0@gmail.com>
2026-09-07 21:46:02 +0800

    fix hardcoded sa account

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
         'login_provider',
         'avatar_url',
